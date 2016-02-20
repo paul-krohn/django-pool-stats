c92e4644216a2eb4d6df0a8fbe9089862edcf985
Paul Krohn <pkrohn@daemonize.com>
2016-02-20 11:02:32 -0800

    add object repr

--- sfpa/stats/models.py
+++ sfpa/stats/models.py
@@ -5,8 +5,14 @@ class Season(models.Model):
     name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date of first week')
 
+    def __str__(self):
+        return self.name
+
 
 class Team(models.Model):
     season = models.ForeignKey(Season, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
