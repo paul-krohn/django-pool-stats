fdc81313d1518b016bc65cc33f5d2156748dccfe
Paul Krohn <pkrohn@daemonize.com>
2016-02-20 10:39:10 -0800

    initial stats app & migrations

--- sfpa/stats/models.py
+++ sfpa/stats/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Season(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date of first week')
+
+
+class Team(models.Model):
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
