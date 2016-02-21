2da41f0d220031de0d72b118489d8f3b6875f8f7
Paul Krohn <pkrohn@daemonize.com>
2016-02-21 09:58:29 -0800

    admin filter teams by season

--- sfpa/stats/admin.py
+++ sfpa/stats/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from .models import Season
+from .models import Season, Team
 
 admin.site.register(Season)
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'season')
+    list_filter = ['season']
+
+admin.site.register(Team, TeamAdmin)
