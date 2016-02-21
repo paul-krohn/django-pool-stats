22df9ac6705d31b43ed55417b6fea2c780938616
Paul Krohn <pkrohn@daemonize.com>
2016-02-21 07:55:16 -0800

    Add field for default season

--- sfpa/stats/models.py
+++ sfpa/stats/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Season(models.Model):
     name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date of first week')
+    is_default = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
