ceda091f58dc7203e9e058d7799ea1a615b4c45d
Paul Krohn <pkrohn@daemonize.com>
2016-03-16 07:36:19 -0700

    add admin drop-down

--- sfpa/sfpa/urls.py
+++ sfpa/sfpa/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^stats/', include('stats.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls, name='admin'),
 ]
