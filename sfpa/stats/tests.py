66043480b412af7a45d0ad3a31ca2480ba34257a
Paul Krohn <pkrohn@daemonize.com>
2016-03-19 17:44:46 -0700

    There are tests! There could be more.

--- sfpa/stats/tests.py
+++ sfpa/stats/tests.py
@@ -1,3 +1,178 @@
+import datetime, time
+from num2words import num2words
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Season, Player, PlayerSeasonSummary, GameOrder, Match, ScoreSheet, Week
+from .models import PlayPosition, AwayPlayPosition, HomePlayPosition
+from .models import Team, AwayTeam, HomeTeam
+
+
+match_ups = [
+    [1, 1],
+    [2, 2],
+    [3, 3],
+
+    [1, 2],
+    [2, 3],
+    [3, 1],
+
+    [1, 3],
+    [2, 1],
+    [3, 2]
+]
+
+
+def create_test_season():
+    a_season = Season(name='Some Future Season', pub_date=timezone.now(), is_default=True)
+    a_season.save()
+    return a_season
+
+
+def create_test_players(count, start_position):
+    inc = 0
+    start_position += 65
+    players = []
+    while inc < count:
+        p = Player(first_name=chr(start_position + inc), last_name=chr(start_position + inc + 26))
+        p.save()
+        players.append(p)
+        inc += 1
+    return players
+
+
+def create_test_teams(a_season):
+    """
+    14 players to populate 2 teams
+    :return:
+    """
+
+    team_a = Team(name="Team A", season=a_season)
+    team_a.save()
+    for p in create_test_players(7, 0):
+        team_a.players.add(p)
+    team_a.save()
+    team_b = Team(name="Team B", season=a_season)
+    team_b.save()
+    for p in create_test_players(7, 16):
+        team_b.players.add(p)
+    team_b.save()
+
+
+def create_test_weeks(a_season, count):
+    weeks = []
+    inc = 0
+    while inc < count:
+        week = Week(
+            season=a_season,
+            date=timezone.now() + datetime.timedelta(days=7 * inc),
+            name='Week {}'.format(chr(65 + inc))
+        )
+        inc += 1
+        week.save()
+        weeks.append(week)
+    return weeks
+
+
+def create_play_positions():
+    positions = []
+    for i in range(1, 4):
+        position = PlayPosition(
+            home_name="Home Player {}".format(chr(65 + i)),
+            away_name="Away Player {}".format(i),
+            name="Player {}".format(num2words(i)),
+        )
+        position.save()
+        positions.append(position)
+    # one sub position
+    positions.append(PlayPosition(
+        home_name="Home Sub 1",
+        away_name="Away Sub 2",
+        name="Sub Two"
+
+    ))
+    return positions
+
+
+def create_game_order():
+    """
+    Use the 3x3 grid in match_up; tests assumptions about 4x4 grid that may lurk in the code
+    :return: bool
+    """
+    game_match_ups = []
+    for match_up in match_ups:
+        game_order = GameOrder(
+            away_position=AwayPlayPosition.objects.get(name='Player {}'.format(num2words(match_up[0]))),
+            home_position=HomePlayPosition.objects.get(name='Player {}'.format(num2words(match_up[1]))),
+            name=num2words(len(game_match_ups) + 1),
+        )
+        game_order.save()
+        game_match_ups.append(game_order)
+    return game_match_ups
+
+
+class SeasonMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future.
+        """
+        future_season = Season(name='Some Future Season', pub_date=timezone.now() + datetime.timedelta(days=180))
+        self.assertEqual(future_season.name, 'Some Future Season')
+
+
+class PlayerViewTests(TestCase):
+
+    def test_player_index(self):
+        """
+        Test that a created player is *not* in the player index; as in many
+        cases, a default season is required
+        """
+        create_test_season()
+
+        player = Player(first_name='George', last_name='Smith')
+        player.save()
+        response = self.client.get(reverse('players'))
+        self.assertQuerysetEqual(response.context['players'], [])
+
+    def test_player_season_summary(self):
+        a_season = create_test_season()
+        player = Player(first_name='George', last_name='Smith')
+        player.save()
+        summary = PlayerSeasonSummary(
+            player=player,
+            season=a_season
+        )
+        summary.save()
+        response = self.client.get(reverse('players'))
+        self.assertQuerysetEqual(response.context['players'], ['<PlayerSeasonSummary: PlayerSeasonSummary object>'])
+
+    def test_player_stats(self):
+        """
+        Create a season, players, 2 teams, a match, a scoresheet, the lineups,
+        then run the team and player updates, then verify the team and player stats are correct
+        :return:
+        """
+        a_season = create_test_season()
+        create_test_teams(a_season)  # we know they are called Team A and Team B
+        create_play_positions()  # we need these in place to test the score sheet creation
+        create_game_order()  # these too ...
+        (week_one, week_two) = create_test_weeks(a_season, 2)
+        match_one = Match(
+            week=week_one,
+            season=a_season,
+            away_team=AwayTeam.objects.get(name='Team A'),
+            home_team=HomeTeam.objects.get(name='Team B'),
+        )
+        match_one.save()
+        response = self.client.get(reverse('score_sheet_create', kwargs={'match_id': match_one.id}))
+        # since we have a fresh DB, assume this will be score sheet number 1 ...
+        self.assertRedirects(response, expected_url=reverse('score_sheet_edit', kwargs={'score_sheet_id': 1}))
+
+        score_sheet = ScoreSheet.objects.get(id=1)
+
+        # the number of games should match the match_ups matrix in create_game_order(), ie 9
+        self.assertEqual(len(score_sheet.games.all()), 9)
+
