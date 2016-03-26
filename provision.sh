62c3ea7a46817d852f496b6f298e1036e26ced30
Paul Krohn <pkrohn@daemonize.com>
2016-03-26 07:46:38 -0700

    python virtualenv

--- provision.sh
+++ provision.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-
+# update all the things
 sudo apt-get update -qq
 sudo apt-get upgrade -y
 
+# packages
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password janet'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password janet'
-sudo apt-get install -y mysql-server python3.4 python3.4-dev python-virtualenv python-pip mysql-client libmysqlclient-dev
+sudo apt-get install -y mysql-common mysql-server mysql-client
+sudo apt-get install -y python3.4 python3.4-dev python-virtualenv python-pip
+sudo apt-get install -y libmysqlclient-dev memcached
 
-rm -r django-pool-stats
-virtualenv django-pool-stats -p python3.4
-. django-pool-stats/bin/activate
+# python virtualenv
+rm -r /vagrant/django-pool-stats-ve
+virtualenv /vagrant/django-pool-stats-ve -p python3.4
+. /vagrant/django-pool-stats-ve/bin/activate
 pip install -r /vagrant/requirements.pip
