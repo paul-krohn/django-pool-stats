8add69a060d8e7447e21c09bbf41a4d6bbe4cf1d
Paul Krohn <pkrohn@daemonize.com>
2016-04-11 16:57:18 -0700

    more scoresheet tests

--- sfpa/stats/tests.py
+++ sfpa/stats/tests.py
@@ -5,10 +5,13 @@ from django.utils import timezone
 from django.test import TestCase
 from django.test import Client
 
-from .models import Season, Player, PlayerSeasonSummary, GameOrder, Match, ScoreSheet, Week
-from .models import PlayPosition, AwayPlayPosition, HomePlayPosition
+from .models import Game, Season, Player, PlayerSeasonSummary, GameOrder, Match, ScoreSheet, Week
+from .models import PlayPosition, AwayPlayPosition, HomePlayPosition, AwayLineupEntry, HomeLineupEntry
 from .models import Team, AwayTeam, HomeTeam
 
+from .views import set_games_for_score_sheet, _count_games, update_players_stats
+
+import random
 
 match_ups = [
     [1, 1],
@@ -36,7 +39,7 @@ def create_players(count, start_position):
     start_position += 65
     players = []
     while inc < count:
-        p = Player(first_name=chr(start_position + inc), last_name=chr(start_position + inc + 26))
+        p = Player(first_name=chr(start_position + inc), last_name=chr(start_position + inc + 32))
         p.save()
         players.append(p)
         inc += 1
@@ -113,6 +116,22 @@ def create_game_order():
     return game_match_ups
 
 
+def populate_lineup_entries(score_sheet):
+    inc = 0
+    for away_lineup_entry in score_sheet.away_lineup.all():
+        away_lineup_entry.player = score_sheet.match.away_team.players.all()[inc]
+        # print(away_lineup_entry.position, away_lineup_entry.player)
+        away_lineup_entry.save()
+        inc += 1
+
+    inc = 0
+    for home_lineup_entry in score_sheet.home_lineup.all():
+        home_lineup_entry.player = score_sheet.match.home_team.players.all()[inc]
+        home_lineup_entry.save()
+        # print(home_lineup_entry.position, home_lineup_entry.player)
+        inc += 1
+
+
 def create_data():
     """
     Create a season, players, 2 teams, and a match, as mock data for many of the tests
@@ -181,3 +200,67 @@ class ScoreSheetTests(TestCase):
         c = Client()
         test_redirect_response = c.get(response.url)
         self.assertRedirects(test_redirect_response, expected_url=reverse('score_sheet', kwargs={'score_sheet_id': 1}))
+
+    def test_score_sheet_set_lineup_with_scored_games(self):
+        """
+        Create a score sheet, create the lineup,
+
+        """
+        test_score_sheet_id = 2
+        response = self.client.get(reverse('score_sheet_create', kwargs={'match_id': self.sample_match_id}))
+        # since we have a fresh DB, assume this will be score sheet number 1 ...
+        self.assertRedirects(
+            response,
+            expected_url=reverse(
+                'score_sheet_edit', kwargs={'score_sheet_id': test_score_sheet_id}
+            )
+        )
+        score_sheet = ScoreSheet.objects.get(id=test_score_sheet_id)
+
+        # creating the score sheet above creates the lineup entries, now we need to populate them
+        populate_lineup_entries(score_sheet)
+        # and the games
+        set_games_for_score_sheet(score_sheet.id)
+        # now we are ready to test some things
+        # in the first game away player should be the team's first player
+        self.assertEqual(score_sheet.games.all()[0].away_player, score_sheet.match.away_team.players.all()[0])
+        # the last game home player should be the second home player
+        self.assertEqual(score_sheet.games.last().home_player, score_sheet.match.home_team.players.all()[1])
+
+        inc = 0
+        # choose a game at random to be the forfeit
+        forfeit_game = random.randint(0, len(match_ups))
+        # same for a table run, not the same as the forfeit ...
+        table_run_game = random.randint(0, len(match_ups))
+        while table_run_game != forfeit_game:
+            table_run_game = random.randint(0, len(match_ups))
+
+        away_wins = 0
+        home_wins = 0
+        away_home = ('away', 'home')
+        for game in score_sheet.games.all():
+            winner = away_home[random.randint(0, 1)]
+            game.winner = winner
+            if winner == 'away':
+                away_wins += 1
+            else:
+                home_wins += 1
+            if inc == table_run_game:
+                game.table_run = True
+            if inc == forfeit_game:
+                game.forfeit = True
+            game.save()
+            inc += 1
+
+        score_sheet.official = True
+        score_sheet.save()
+
+        _count_games(score_sheet.match.away_team)
+        _count_games(score_sheet.match.home_team)
+
+        # forfeit wins count for teams, _count_games does not omit them.
+        self.assertEqual(score_sheet.match.away_team.wins(), away_wins)
+        self.assertEqual(score_sheet.match.home_team.wins(), home_wins)
+
+        # needs refactor of the function to a class method to test
+        update_players_stats(response)
