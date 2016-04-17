bb02a83209237cdfe0048fbe22d32a1a59776897
Paul Krohn <pkrohn@daemonize.com>
2016-04-16 22:20:57 -0700

    Update provisioning to include mysql user

--- Vagrantfile
+++ Vagrantfile
@@ -13,6 +13,6 @@ Vagrant.configure(2) do |config|
   config.vm.provision "shell",
     inline: "/bin/bash /vagrant/provision.sh"
 
-  config.vm.network "forwarded_port", guest: 8001, host: 8001
+  config.vm.network "forwarded_port", guest: 8000, host: 8000
 
 end
--- provision.sh
+++ provision.sh
@@ -11,8 +11,31 @@ sudo apt-get install -y mysql-common mysql-server mysql-client
 sudo apt-get install -y python3.4 python3.4-dev python-virtualenv python-pip
 sudo apt-get install -y libmysqlclient-dev memcached
 
+VE_DIR="/usr/local/django-pool-stats-ve"
+
+/bin/echo ". ${VE_DIR}/bin/activate" > ~vagrant/.bash_profile
+chown vagrant:vagrant
+
 # python virtualenv
-rm -r /vagrant/django-pool-stats-ve
-virtualenv /vagrant/django-pool-stats-ve -p python3.4
-. /vagrant/django-pool-stats-ve/bin/activate
+if [ ! -d ${VE_DIR}] ] ; then
+    sudo virtualenv ${VE_DIR} -p python3.4
+    sudo chown -R vagrant:vagrant ${VE_DIR}
+fi
+. ${VE_DIR}/bin/activate
 pip install -r /vagrant/requirements.pip
+
+cat <<MYSQL_USER | mysql -u root -pjanet
+GRANT ALL ON sfpa_stats.* TO 'sfpa_django'@'localhost' identified by 'isysroot';
+create database if not exists sfpa_stats;
+flush privileges;
+MYSQL_USER
+
+cat <<EOF > ~vagrant/runserver.sh
+cd /vagrant/sfpa
+python manage.py runserver 0.0.0.0:8000
+EOF
+
+#echo ${RUNIT} >> ~vagrant/runserver.sh
+chown vagrant:vagrant ~vagrant/runserver.sh
+chmod +x ~vagrant/runserver.sh
+
