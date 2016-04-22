a9c6e6200324de82f30cf26ebe8a2974fef63d4f
Paul Krohn <pkrohn@daemonize.com>
2016-04-22 07:09:11 -0700

    Sort players by first name

--- pool/stats/models.py
+++ pool/stats/models.py
@@ -27,6 +27,11 @@ class Player(models.Model):
     def __str__(self):
         return self.display_name or "%s %s" % (self.first_name, self.last_name)
 
+    class Meta:
+        # Players with a display_name that sorts differently from their
+        # first name will appear to be out of order in form selects.
+        ordering = ['first_name', 'last_name']
+
 
 class PlayerSeasonSummary(models.Model):
     player = models.ForeignKey(Player)
