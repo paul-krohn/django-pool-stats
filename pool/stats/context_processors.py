f5e1143de6c2c6f4076856234715f0a09b29bf1a
Paul Krohn <pkrohn@daemonize.com>
2016-05-20 22:15:45 -0700

    fix setting season in session

--- pool/stats/context_processors.py
+++ pool/stats/context_processors.py
@@ -2,7 +2,11 @@ from .models import Season
 
 
 def season(request):
+    try:
+        _season = Season.objects.get(id=request.session['season_id'])
+    except KeyError:
+        _season = Season.objects.get(is_default=True)
     return {
         'seasons': Season.objects.order_by('-pub_date')[0:4],
-        'season': Season.objects.get(id=request.session['season_id'])
+        'season': _season
     }
