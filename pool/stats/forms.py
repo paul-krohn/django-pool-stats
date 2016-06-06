aca22eb610fc151c9164bdda60ea9f0fb8f17ba3
Paul Krohn <pkrohn@daemonize.com>
2016-06-06 05:35:39 -0700

    validates, but shows all players in the drop-down

--- pool/stats/forms.py
+++ pool/stats/forms.py
@@ -46,3 +46,24 @@ class ScoreSheetCompletionForm(django.forms.ModelForm):
     class Meta:
         model = ScoreSheet
         fields = ['complete', 'comment']
+
+
+class AwayLineupFormSet(django.forms.BaseModelFormSet):
+    def clean(self):
+        super(AwayLineupFormSet, self).clean()
+
+        player_values = []
+        for form in self.forms:
+            print(form.cleaned_data)
+            if 'player' in form.cleaned_data.keys() and form.cleaned_data['player'] is not None:
+                player_values.append(form.cleaned_data['player'])
+        if len(player_values) > len(set(player_values)):
+            message = 'all players must be unique; there were {} players but only {} unique ones'.format(
+                len(player_values), len(set(player_values))
+            )
+            print(message)
+            raise ValidationError(message)
+        else:
+            print("away lineup formset looks good")
+
+
