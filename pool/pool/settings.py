a275242129dbe0afc207d0e97d0f282c8eeae0b1
Paul Krohn <pkrohn@daemonize.com>
2016-09-03 09:36:44 -0700

    set sql strict mode

--- pool/pool/settings.py
+++ pool/pool/settings.py
@@ -83,6 +83,9 @@ DATABASES = {
         'NAME': 'pool_stats',
         'USER': 'pool_stats',
         'PASSWORD': 'isysroot',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
