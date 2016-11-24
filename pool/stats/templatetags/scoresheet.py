6e465f88b359925710ad071fd3b9982b90f6d71c
Paul Krohn <pkrohn@daemonize.com>
2016-11-24 07:13:48 -0800

    avoid bad str-int conversions on row order

--- pool/stats/templatetags/scoresheet.py
+++ pool/stats/templatetags/scoresheet.py
@@ -5,11 +5,16 @@ register = template.Library()
 
 
 @register.filter()
-def row_even_odd(game_number):
+def row_even_odd(game_order):
     """Returns even or odd for the row for styling purposes"""
-    game_number = int(game_number)
+    if game_order.tiebreaker:
+        return 'even'
+    try:
+        game_number = int(game_order.name)
+    except ValueError:
+        game_number = 2
     if ((game_number - 1) % settings.LEAGUE['game_group_size'] + 1) % 2:
-        return "odd"
+        return 'odd'
     else:
-        return "even"
+        return 'even'
 
