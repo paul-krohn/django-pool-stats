a292afe1b15226b2674c346be0b615b00a1d7655
Paul Krohn <pkrohn@daemonize.com>
2017-08-18 21:24:19 -0700

    catch 2 exceptions

--- pool/stats/context_processors.py
+++ pool/stats/context_processors.py
@@ -5,8 +5,7 @@ from django.conf import settings
 def season(request):
     try:
         _season = Season.objects.get(id=request.session['season_id'])
-    # this exception used to be on KeyError, was the difference related to switch to postgres somehow?
-    except Season.DoesNotExist:
+    except (Season.DoesNotExist, KeyError):
         _season = Season.objects.get(is_default=True)
     return {
         'seasons': Season.objects.order_by('-pub_date')[0:4],
