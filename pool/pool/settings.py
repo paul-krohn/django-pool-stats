1c2c40adee579335c74e536d5688c649a585ea5f
Paul Krohn <pkrohn@daemonize.com>
2018-02-27 06:50:41 -0800

    add never-cache middleware

--- pool/pool/settings.py
+++ pool/pool/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'stats.middleware.downstream_caching.Disable',
 ]
 
 ROOT_URLCONF = 'pool.urls'
