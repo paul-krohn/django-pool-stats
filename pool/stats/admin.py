458574354353e5e558761855cb17451f95fcaaf7
Paul Krohn <pkrohn@daemonize.com>
2018-05-03 21:17:02 -0700

    make player summary updating coarser but more accurate

--- pool/stats/admin.py
+++ pool/stats/admin.py
@@ -315,15 +315,9 @@ def update_stats(modeladmin, request, queryset):
         for team in [score_sheet.match.home_team, score_sheet.match.away_team]:
             team.count_games()
             expire_page(request, reverse('team', kwargs={'team_id': team.id}), '')
-        players = [x.player for x in list(score_sheet.away_lineup.all()) + list(score_sheet.home_lineup.all())]
-        for substitution in list(score_sheet.away_substitutions.all()) + list(score_sheet.home_substitutions.all()):
-            players.append(substitution.player)
-        for player in players:
-            if player is None:
-                continue
-            summary = PlayerSeasonSummary.objects.get_or_create(player=player, season=score_sheet.match.season)[0]
-            summary.update()
-            expire_page(request, reverse('player', kwargs={'player_id': player.id}), '')
+    PlayerSeasonSummary.update_all(season_id=expire_season_id)
+    for pss in PlayerSeasonSummary.objects.filter(season_id=expire_season_id):
+        expire_page(request, reverse('player', kwargs={'player_id': pss.player.id}))
     Team.update_rankings(season_id=expire_season_id)
     expire_page(request, reverse('divisions', kwargs={'season_id': expire_season_id}), '')
     expire_page(request, reverse('players', kwargs={'season_id': expire_season_id}), '')
