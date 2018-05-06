846983289a7c4fcbbd071c215f9308f3adfc934e
Paul Krohn <pkrohn@daemonize.com>
2018-05-06 06:26:51 -0700

    factor out game model

--- pool/stats/models/__init__.py
+++ pool/stats/models/__init__.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 
 from .division import Division
+from .game import Game
 from .lineup import GameOrder, LineupEntry, AwayLineupEntry, HomeLineupEntry
 from .lineup import Substitution, AwaySubstitution, HomeSubstitution
 from .player import Player, AwayPlayer, HomePlayer
@@ -470,27 +471,6 @@ class Match(models.Model):
     class Meta:
         verbose_name = 'Match'
         verbose_name_plural = 'Matches'
-
-
-class Game(models.Model):
-    away_player = models.ForeignKey(
-        AwayPlayer,
-        null=True,
-        blank=True,
-        related_name='away_player',
-        on_delete=models.CASCADE,
-    )
-    home_player = models.ForeignKey(
-        HomePlayer,
-        null=True,
-        blank=True,
-        related_name='home_player',
-        on_delete=models.CASCADE,
-    )
-    winner = models.CharField(max_length=4, blank=True)
-    order = models.ForeignKey(GameOrder, null=True, on_delete=models.CASCADE)
-    table_run = models.BooleanField(default=False)
-    forfeit = models.BooleanField(default=False)
 
 
 class ScoreSheet(models.Model):
