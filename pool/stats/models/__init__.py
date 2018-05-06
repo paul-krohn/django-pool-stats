86ec10deb5371914ac7712aaee9a52231a80a89a
Paul Krohn <pkrohn@daemonize.com>
2018-05-06 06:03:43 -0700

    breakout play positions

--- pool/stats/models/__init__.py
+++ pool/stats/models/__init__.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
+
+from .playposition import PlayPosition, AwayPlayPosition, HomePlayPosition
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -503,32 +506,6 @@ class Match(models.Model):
     class Meta:
         verbose_name = 'Match'
         verbose_name_plural = 'Matches'
-
-
-class PlayPosition(models.Model):
-    home_name = models.CharField(max_length=16)
-    away_name = models.CharField(max_length=16)
-    name = models.CharField(max_length=16)
-    tiebreaker = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
-
-
-class AwayPlayPosition(PlayPosition):
-    class Meta:
-        proxy = True
-
-    def __str__(self):
-        return self.away_name
-
-
-class HomePlayPosition(PlayPosition):
-    class Meta:
-        proxy = True
-
-    def __str__(self):
-        return self.home_name
 
 
 class AwayPlayer(Player):
