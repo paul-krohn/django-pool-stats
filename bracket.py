c883085e697a4d5011280ef23db47d11d874b170
Paul Krohn <pkrohn@daemonize.com>
2018-06-18 08:52:07 -0700

    very basic single elim

--- bracket.py
+++ bracket.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+from copy import deepcopy
 from math import log
 
 BRACKET_TYPES = ['single', 'double']  # [0] is the default
@@ -37,20 +38,27 @@ def bracket_size(participant_count):
 
 class Match(object):
 
-    def __init__(self, source_match_a, source_match_b, team_a, team_b):
+    def __init__(self, source_match_a, source_match_b, team_a, team_b, id):
         self.source_match_a = source_match_a
         self.source_match_b = source_match_b
         self.team_a = team_a
         self.team_b = team_b
         self.winner = None
+        self.id = id
 
-    def __str__(self):
-        return '{} vs {}'.format(self.team_a, self.team_b)
+    def teams_desc(self, side):
+        description = "winner of {} vs {}".format(self.team_a, self.team_b)
+        src_m = getattr(self, 'source_match_{}'.format(side), None)
+        if src_m is not None:
+            description = "winner of match {}".format(src_m.id)
+        return description
 
     def __repr__(self):
-        return("{} vs {}".format(
-            self.team_a or "winner of {}".format(self.source_match_a),
-            self.team_b or "winner of {}".format(self.source_match_b),
+        # print("repr fired")
+        return("match {}: {} vs {}".format(
+            self.id,
+            self.team_a or self.source_match_a.teams_desc('a'),
+            self.team_b or self.source_match_b.teams_desc('b'),
         ))
 
 
@@ -68,14 +76,16 @@ while i < (br_size / 2) + 1:
             source_match_b=None,
             team_a='Team {}'.format(i),
             team_b='Team {}'.format(br_size - (i - 1)),
+            id=i,
         )
     )
     i += 1
 
+running_match_count = i
 match_objects.append(first_round_match_objects)
 
 
-def new_round_object_matches(existing_round_matches):
+def new_round_object_matches(existing_round_matches, offset):
     these_matches = []
     inc = 0
     while inc < len(existing_round_matches) / 2:
@@ -85,17 +95,24 @@ def new_round_object_matches(existing_round_matches):
                 source_match_b=existing_round_matches[int(len(existing_round_matches) - inc - 1)],
                 team_a=None,
                 team_b=None,
+                id=offset + inc,
             )
-
         )
         inc += 1
     return these_matches
 
 
-prev_round_match_objects = first_round_match_objects
-while len(match_objects) < int(log(br_size, 2)):
-    this_round_match_objects = new_round_object_matches(prev_round_match_objects)
-    match_objects.append(this_round_match_objects)
-    prev_round_matches = this_round_match_objects
+round_count = int(log(br_size, 2))
 
-print(match_objects)
+prev_round_match_objects = first_round_match_objects
+while len(match_objects) < round_count:
+    this_round_match_objects = new_round_object_matches(
+        prev_round_match_objects, running_match_count
+    )
+    running_match_count += len(this_round_match_objects)
+    match_objects.append(this_round_match_objects)
+    prev_round_match_objects = this_round_match_objects
+
+for match_object_list in match_objects:
+    for match_object in match_object_list:
+        print(match_object)
