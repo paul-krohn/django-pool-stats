3ad71ab3c1c68ec8a73b4623290786ec7dc28df3
Paul Krohn <pkrohn@daemonize.com>
2018-07-08 06:31:47 -0700

    first-round matches

--- pool/stats/models/tournament.py
+++ pool/stats/models/tournament.py
@@ -1,4 +1,5 @@
 from django.db import models
+from math import ceil, log
 
 
 TOURNAMENT_TYPES = [
@@ -14,6 +15,11 @@ ELIMINATION_TYPES = [
 BRACKET_TYPES = [
     ('w', 'Winners'),
     ('l', 'Losers'),
+]
+
+ROUND_TYPES = [
+    'elimination',
+    'drop_in',
 ]
 
 # these do/must match the field names in Participant, below
@@ -40,6 +46,57 @@ class Tournament(models.Model):
             losers_bracket = Bracket(tournament=self, type='l')
             losers_bracket.save()
 
+    def round_count(self):
+        return ceil(log(len(self.participant_set.all()), 2))
+
+    def bracket_size(self):
+        return 2 ** self.round_count()
+
+    def create_rounds(self):
+        round_inc = 0
+        while round_inc < self.round_count():
+            wbr = Round(
+                bracket=self.bracket_set.get(type='w'),
+                number=round_inc + 1,
+            )
+            wbr.save()
+            round_inc += 1
+        if self.elimination == 'double':
+            losers_bracket_round_inc = 0
+            losers_bracket_round_count = 2 * (self.round_count() - 1)
+            while losers_bracket_round_inc < losers_bracket_round_count:
+                lbr = Round(
+                    bracket=self.bracket_set.get(type='l'),
+                    number=round_inc + 1,
+                )
+                lbr.save()
+                losers_bracket_round_inc += 1
+
+    def create_matchups(self, side='w'):
+
+        # first round winners side matchups; assumes participant_set is in the desired order
+        i = 0
+        br_size = self.bracket_size()
+
+        while i < (br_size / 2):
+            participant_one = self.participant_set.all()[i]
+            participant_two = False
+            if len(self.participant_set.all()) > (br_size - 1) - i:
+                participant_two = self.participant_set.all()[(br_size - 1) - i]
+                # TournamentMatchup.objects.get_or_create()
+            tm, created = TournamentMatchup.objects.get_or_create(
+                source_match_a=None,
+                source_match_b=None,
+                participant_a=participant_one,
+                participant_b=participant_two,
+                round=self.bracket_set.get(type=side).round_set.get(number=1),
+                number=i + 1,
+            )
+            print("matchup {} vs {}:  was created: {}".format(
+                tm.participant_a, tm.participant_b, created)
+            )
+            tm.save()
+            i += 1
 
 class Participant(models.Model):
     type = models.TextField(choices=PARTICIPANT_TYPES)
@@ -92,9 +149,9 @@ class TournamentMatchup(models.Model):
     )
 
     number = models.IntegerField()
-    a_want_winner = models.BooleanField()
-    b_want_winner = models.BooleanField()
-    play_order = models.IntegerField()
+    a_want_winner = models.NullBooleanField(null=True)
+    b_want_winner = models.NullBooleanField(null=True)
+    play_order = models.IntegerField(null=True)
 
     def description(self, side, want):
         if self.participant_a and self.participant_b:
