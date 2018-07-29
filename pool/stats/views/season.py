4396df36c08bad16af3174714ce10b93e5b1af34
Paul Krohn <pkrohn@daemonize.com>
2018-07-29 11:30:40 -0700

    clean up season set/check

--- pool/stats/views/season.py
+++ pool/stats/views/season.py
@@ -1,6 +1,7 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, reverse
 
 from ..models import Season
+# from ..views.team import teams
 
 
 def get_default_season():
@@ -28,14 +29,16 @@ def set_season(request, season_id=None):
     request.session['season_id'] = season_id
     request.session.save()
     # hard-coded urls are bad okay?
-    return redirect(request.META.get('HTTP_REFERER', '/stats/'))
+    redirect_to = '/stats/'
+    if season_id:
+        redirect_to = reverse('teams', kwargs={'season_id': season_id})
+    return redirect(redirect_to)
 
 
 def check_season(request):
-    if 'season_id' in request.session:
-        return
-    else:
-        set_season(request)
+    if 'season_id' not in request.session:
+        request.session['season_id'] = get_default_season()
+        request.session.save()
 
 
 def seasons(request):
