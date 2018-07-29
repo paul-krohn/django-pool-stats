f536724611c1f1f5434388b6fb3fa95944ba45d9
Paul Krohn <pkrohn@daemonize.com>
2018-07-29 06:47:40 -0700

    new weeks get default season

--- pool/stats/views/season.py
+++ pool/stats/views/season.py
@@ -3,6 +3,19 @@ from django.shortcuts import redirect, render
 from ..models import Season
 
 
+def get_default_season():
+    default_season_id = 0
+    try:
+        default_season_id = Season.objects.get(is_default=True).id
+    except Season.DoesNotExist as e:
+        # there is no default season, try to get the last/latest one; get them in descending order, as
+        # negative indexing does not work on querysets.
+        the_seasons = Season.objects.order_by('-pub_date')
+        if len(the_seasons):
+            default_season_id = the_seasons[0].id
+    return default_season_id
+
+
 def set_season(request, season_id=None):
     """
     Allow the user to set their season to a value other than the default.
@@ -11,7 +24,7 @@ def set_season(request, season_id=None):
     :return: bool
     """
     if season_id is None:
-        season_id = Season.objects.get(is_default=True).id
+        season_id = get_default_season()
     request.session['season_id'] = season_id
     request.session.save()
     # hard-coded urls are bad okay?
