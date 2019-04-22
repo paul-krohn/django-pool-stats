a275b8715b8fbe397e1e5414ba8e1d731f72217e
Paul Krohn <pkrohn@daemonize.com>
2019-04-21 17:17:30 -0700

    refactor elo calculation, maybe use a function here and there

--- pool/stats/management/commands/update-elo.py
+++ pool/stats/management/commands/update-elo.py
@@ -5,7 +5,7 @@ from elo import Rating, rate_1vs1
 from django.core.management.base import BaseCommand
 from django.db import models
 
-from stats.models import Game, PlayerSeasonSummary, Season
+from stats.models import Game, PlayerSeasonSummary, ScoreSheet, Season
 from stats.models.globals import away_home
 
 logger = logging.getLogger('stats')
@@ -31,14 +31,11 @@ def get_old_elo(player_summary):
             previous_season_summary = PlayerSeasonSummary.objects.filter(
                 player=player_summary.player, season=previous_season
             )
-            print(previous_season_summary)
             if len(previous_season_summary):
                 previous_elo = previous_season_summary[0].elo
                 print("previous elo for {} ({}): {}".format(
                     previous_season_summary[0], previous_season_summary[0].player.id, previous_elo
                 ))
-            # else:
-            #     print('no summary for season {} player {}'.format(previous_season, player_summary.player))
     return Rating(previous_elo)
 
 
@@ -46,60 +43,102 @@ class Command(BaseCommand):
     help = 'Calculates players\' ELO rating'
 
     def add_arguments(self, parser):
-        parser.add_argument('season_id', type=int)
+        parser.add_argument('the_id', type=int)
+        parser.add_argument('--type', type=str, default='season')
+
+    @staticmethod
+    def get_games_filter(options):
+        main_filter_args = {
+            'scoresheet__official': True,
+            'scoresheet__match__playoff': False,
+            'forfeit': False
+        }
+        if options['type'] == 'scoresheet':
+            main_filter_args['scoresheet__id'] = options['the_id']
+        elif options['type'] == 'season':
+            main_filter_args['scoresheet__match__season_id'] = options['the_id']
+        return main_filter_args
+
+    @staticmethod
+    def get_summaries(game, season_id):
+        summaries = dict()
+        for ah in away_home:
+            player = getattr(game, '{}_player'.format(ah))
+            try:
+                summaries[ah] = PlayerSeasonSummary.objects.get(
+                    season_id=season_id, player=player
+                )
+            except PlayerSeasonSummary.DoesNotExist as e:
+                print(
+                    "no season summary for {}({}) based on game id {}. error: {}".format(player, player.id, game.id, e))
+        return summaries
+
+    def update_elo_from_game(self, game, season_id):  # summaries, winner_index, loser_index):
+        # if game.away_player is None or game.home_player is None:
+        #     return False
+        wl = ['winner', 'loser']
+        summaries = self.get_summaries(game, season_id)
+        winner = game.winner
+
+        # if the winner is 'home'
+        winner_index = away_home.index(winner)
+        loser_index = 1 - away_home.index(winner)
+        new_elos = {}
+        # print(new_elos.get('winner'))
+        # new_winner, new_loser = rate_1vs1(
+        new_elos['winner'], new_elos['loser'] = rate_1vs1(
+            Rating(get_old_elo(summaries[away_home[winner_index]])),
+            Rating(get_old_elo(summaries[away_home[loser_index]])),
+        )
+
+        game.away_elo = new_elos[wl[winner_index]]
+        game.home_elo = new_elos[wl[loser_index]]
+        game.save()
+
+        summaries[away_home[winner_index]].elo = new_elos['winner']
+        summaries[away_home[loser_index]].elo = new_elos['loser']
+
+        summaries[away_home[winner_index]].save()
+        summaries[away_home[loser_index]].save()
+
+        if self.verbosity >= 3:
+            self.stdout.write("{} def {} new ratings: {:.1f}, {:.1f}".format(
+                summaries[away_home[winner_index]].player,
+                summaries[away_home[loser_index]].player,
+                new_elos['winner'],
+                new_elos['loser'])
+            )
 
     def handle(self, *args, **options):
 
         # your linter may flag this as outside of init, but that's ok, `handle()` is as close as we'll get to `__init__()`.
         self.verbosity = options['verbosity']
 
-        season_id = options['season_id']
+        assert options['type'] in ['scoresheet', 'season']
 
+        season_id = options['the_id']
+        if options['type'] == 'scoresheet':
+            season_id = ScoreSheet.objects.get(id=options['the_id']).match.season.id
+        print('the season id is {}'.format(season_id))
+        main_filter_args = self.get_games_filter(options)
+        print("filter args are: {}".format(main_filter_args))
         games = Game.objects.filter(
-            scoresheet__match__season_id=season_id
-        ).filter(
-            scoresheet__official=True
-        ).filter(
-            scoresheet__match__playoff=False
-        ).filter(
-            forfeit=False
+            **main_filter_args
         ).exclude(
             winner=''
+        ).filter(
+            away_elo=None
+        ).filter(
+            home_elo=None
+        ).exclude(
+            winner=None
+        ).exclude(
+            away_player=None
+        ).exclude(
+            home_player=None
         )
-        count = 0
+        # count = 0
         for game in games:
-            count += 1
-            if game.away_player is None or game.home_player is None:
-                continue
-            summary = dict()
-            for ah in away_home:
-                player = getattr(game, '{}_player'.format(ah))
-                try:
-                    summary[ah] = PlayerSeasonSummary.objects.get(season_id=season_id, player=player)
-                except PlayerSeasonSummary.DoesNotExist as e:
-                    print("no season summary for {} based on game id {}. error: {}".format(player, game.id, e))
+            # print(game.id)
+            self.update_elo_from_game(game, season_id)
 
-            winner = game.winner
-
-            # if the winner is 'home'
-            winner_index = away_home.index(winner)
-            loser_index = 1 - away_home.index(winner)
-
-            new_winner, new_loser = rate_1vs1(
-                Rating(get_old_elo(summary[away_home[winner_index]])),
-                Rating(get_old_elo(summary[away_home[loser_index]])),
-            )
-
-            summary[away_home[winner_index]].elo = new_winner
-            summary[away_home[loser_index]].elo = new_loser
-
-            summary[away_home[winner_index]].save()
-            summary[away_home[loser_index]].save()
-
-            if self.verbosity >= 3:
-                self.stdout.write("{} def {} new ratings: {}, {}".format(
-                    summary[away_home[winner_index]].player,
-                    summary[away_home[loser_index]].player,
-                    new_winner,
-                    new_loser)
-                )
