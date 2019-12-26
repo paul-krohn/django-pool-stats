c6d571b05e5f1b8f2fc793bca43408cd682f71b5
Paul Krohn <pkrohn@daemonize.com>
2019-12-26 09:41:42 -0800

    add sponsor sort_name field

--- pool/stats/models/sponsor.py
+++ pool/stats/models/sponsor.py
@@ -3,8 +3,16 @@ from django.db import models
 
 class Sponsor(models.Model):
     name = models.CharField(max_length=200)
+    sort_name = models.TextField(blank=True, max_length=200)
     address = models.CharField(max_length=200)
     link = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.sort_name = self.name.replace('The ', '')
+        super(Sponsor, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['sort_name']
